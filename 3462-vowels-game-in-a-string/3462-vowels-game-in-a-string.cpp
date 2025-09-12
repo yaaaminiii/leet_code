13ce4622a594af6bc797666499cf80a33c98e6cb
yaaaminiii <yaminibudala29@gmail.com>
2025-09-12 21:17:13 +0530

    Time: 5 ms (67.72%), Space: 19 MB (53.73%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count=0;
+        for(int i=0;i<s.size();i++){
+        if(s[i]=='a' || s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+        {
+            count++;
+        }}
+        if(count>0){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+};
