2bd67597cc8f862ee58263da42be93812ebc53ae
yaaaminiii <yaminibudala29@gmail.com>
2025-09-18 11:43:23 +0530

    Time: 0 ms (100%), Space: 8.2 MB (69.72%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+    
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<i;j++){
+                if(s[i]==s[j])
+                return s[i];
+            }
+        }
+        return ' ';
+    }
+};
