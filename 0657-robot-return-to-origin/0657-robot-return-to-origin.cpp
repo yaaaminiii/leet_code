7ed660b1604b632b1fb37e4ab363e1570bfcf02c
yaaaminiii <yaminibudala29@gmail.com>
2025-12-02 11:31:45 +0530

    Time: 2 ms (54.06%), Space: 10.4 MB (45.45%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        map<char,int>mpp;
+        /*map<char,int>mpp;
         for(auto i: moves){
             mpp[i]++;
         }
@@ -10,6 +10,27 @@ public:
         }
         else{
             return false;
+        }*/
+      int x=0,y=0;
+      for(int i=0;i<moves.size();i++){
+        if(moves[i]=='U'){
+            x--;
         }
+        else if(moves[i]=='D'){
+            x++;
+        }
+        else if(moves[i]=='L'){
+            y--;
+        }
+        else{
+            y++;
+        }
+      }
+      if(x==0 && y==0){
+        return true;
+      }
+      else{
+        return false;
+      }
     }
 };
