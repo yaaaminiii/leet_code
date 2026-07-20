e2cae29698d78c1c2ea831fbc572c1891de3c29d
yaaaminiii <yaminibudala29@gmail.com>
2026-07-20 15:10:50 +0530

    Time: 4 ms (20.17%), Space: 10.5 MB (33.6%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -2,15 +2,25 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int l=0,r=s.size()-1;
-        while(l<r){
+        /*while(l<r){
         while(l<r && !(s[l]=='a' || s[l]=='e' || s[l]=='i'||s[l]=='o'||s[l]=='u'||s[l]=='A'||s[l]=='E'||s[l]=='I' ||s[l]=='O'||s[l]=='U')) l++;
-         while(l<r &&!(s[r]=='a' || s[r]=='e' || s[r]=='i'||s[r]=='o'||s[r]=='u'||s[r]=='A'||s[r]=='E'||s[r]=='I' ||s[r]=='O'||s[r]=='U')) r--;
+         while(l<r && !(s[r]=='a' || s[r]=='e' || s[r]=='i'||s[r]=='o'||s[r]=='u'||s[r]=='A'||s[r]=='E'||s[r]=='I' ||s[r]=='O'||s[r]=='U')) r--;
                 if(l<r){
                 swap(s[l],s[r]);
                 l++;
                 r--;
                 }
+        }*/
+      string vowels="aeiouAEIOU";
+      while(l<r){
+        while(l<r && vowels.find(s[l])==string::npos)l++; 
+        while(l<r && vowels.find(s[r])==string::npos)r--;
+        if(l<r){ 
+            swap(s[l],s[r]);
+            l++;   
+            r--;
         }
+      }
     return s;
     }
 };
