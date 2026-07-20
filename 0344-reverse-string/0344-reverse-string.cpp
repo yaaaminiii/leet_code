0b7097aac19e7ba386a2d97eed7acdb5004ab75d
yaaaminiii <yaminibudala29@gmail.com>
2026-07-20 14:37:30 +0530

    Time: 0 ms (100%), Space: 27.1 MB (98.71%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,7 +3,10 @@ public:
     void reverseString(vector<char>& s) {
        int left=0,right=s.size()-1;
        while(left<right){
-        swap(s[left],s[right]);
+        //swap(s[left],s[right]);
+        char temp=s[left];
+         s[left]=s[right];
+         s[right]=temp;
             left++;
             right--;
         }
