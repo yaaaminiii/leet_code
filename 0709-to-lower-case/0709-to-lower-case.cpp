e16427dc5355a941ba839c8dfcfbb4354469235d
yaaaminiii <yaminibudala29@gmail.com>
2026-07-20 12:32:18 +0530

    Time: 0 ms (100%), Space: 8.4 MB (16.46%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-       for(char &c:s){
-        c=tolower(c);
-       }
-       return s;
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='A' && s[i]<='Z'){
+            s[i]=s[i]+32;}
+        }
+        return s;
     }
 };
