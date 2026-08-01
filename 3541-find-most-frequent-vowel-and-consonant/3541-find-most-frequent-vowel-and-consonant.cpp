532dc6125998b3d0c62ed43c1aa38812ee9aa225
yaaaminiii <yaminibudala29@gmail.com>
2026-08-01 12:07:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.43%) - LeetHub

---
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        map<char,int>freq;
+        for(char ch:s){
+            freq[ch]++;
+        }
+        string v="aeiou";
+        int max_vowel=0;
+        int max_consonant=0;
+
+        for(int i=0;i<s.size();i++){
+            if(v.find(s[i])==string::npos){
+                max_consonant=max(max_consonant,freq[s[i]]);
+            }
+            else{
+                max_vowel=max(max_vowel,freq[s[i]]);
+            }
+        }
+        return max_vowel+max_consonant;
+
+
+                
+     
+    }
+};
