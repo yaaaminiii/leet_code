cdc67e1b8ae03e59c660dc849932fd90905726c7
yaaaminiii <yaminibudala29@gmail.com>
2026-09-08 14:44:17 +0530

    Time: 2 ms (15.38%), Space: 12.9 MB (16.66%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
     int calPoints(vector<string>& operations) {
-        vector<int>result;
-        for( int i=0;i< operations.size();i++){
-            if(operations[i]=="+"){
-                result.push_back(result[result.size()-1]+result[result.size()-2]);
-            }
-            else if(operations[i] =="D"){
-                result.push_back(2*result.back());
-            }
-            else if(operations[i]=="C"){
-                result.pop_back();
-            }
-            else{
-                result.push_back(stoi(operations[i]));
-            }
+        // vector<int>result;
+        // for( int i=0;i< operations.size();i++){
+        //     if(operations[i]=="+"){
+        //         result.push_back(result[result.size()-1]+result[result.size()-2]);
+        //     }
+        //     else if(operations[i] =="D"){
+        //         result.push_back(2*result.back());
+        //     }
+        //     else if(operations[i]=="C"){
+        //         result.pop_back();
+        //     }
+        //     else{
+        //         result.push_back(stoi(operations[i]));
+        //     }
+        // }
+        // int sum=0;
+        // for(int i=0;i<result.size();i++){
+        //     sum+=result[i];
+        // }
+        // return sum;
+     stack<int>st;
+     for(int i=0;i<operations.size();i++){
+        if(operations[i]=="C"){
+            st.pop();
         }
-        int sum=0;
-        for(int i=0;i<result.size();i++){
-            sum+=result[i];
+        else if (operations[i] == "D") {
+                st.push(2 * st.top());
+            }
+        else if(operations[i]=="+"){
+            int first=st.top();
+            st.pop();
+            int second=st.top();
+            st.push(first);
+            st.push(first+second);
+            //st.push(first);
         }
-        return sum;
-
+        else{
+            st.push(stoi(operations[i]));
+        }
+     }
+     int sum=0;
+      while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
+return sum;
         
     }
 };
